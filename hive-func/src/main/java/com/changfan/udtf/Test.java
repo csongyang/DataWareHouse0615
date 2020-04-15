be944e77fbd4ecf8f0243145416afceaa152d58d
sy <sy.@c.com>
2020-04-15 22:56:22 +0800

    添加一个方法

---
+++ hive-func/src/main/java/com/changfan/udtf/Test.java
@@ -0,0 +1,12 @@
+package com.changfan.udtf;
+
+/**
+ * author: jiaozhu
+ * create: 2020-04-15-22:55
+ * tel: 17717876906
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
