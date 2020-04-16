11acb70cb93cb383f81eb304345ea174cb5ce32c
sy <sy.@c.com>
2020-04-16 13:55:00 +0800

    测试

--- hive-func/src/main/java/com/changfan/udtf/Test.java
+++ hive-func/src/main/java/com/changfan/udtf/Test.java
@@ -7,6 +7,6 @@ package com.changfan.udtf;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello java");
     }
 }
